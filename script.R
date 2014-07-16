6298644be1c381172b64d583087cc7c4c5031585
herchu1 <hfoffani+datasciencecoursera@gmail.com>
2014-07-16 18:40:36 +0200

    Better X and Y axis labels.

--- script.R
+++ script.R
@@ -6,13 +6,18 @@ activity$date <- as.Date(activity$date)
 
 stepsbydate <- aggregate(steps ~ date, data=activity, FUN=sum)
 ggplot(stepsbydate, aes(x=date, y=steps)) +
-    geom_histogram(alpha=.5, stat="identity")
+    geom_histogram(alpha=.5, stat="identity") +
+    ylab('Number of Steps') +
+    xlab('Date')
 mean(stepsbydate$steps, na.rm=T)
 median(stepsbydate$steps, na.rm=T)
 
 
 stepsbyinterval <- aggregate(steps ~ interval, data=activity, FUN=mean)
-ggplot(stepsbyinterval, aes(x=interval, y=steps)) + geom_line()
+ggplot(stepsbyinterval, aes(x=interval, y=steps)) +
+    geom_line() +
+    ylab('Number of Steps') +
+    xlab('Interval')
 maxrow <- stepsbyinterval$steps == max(stepsbyinterval$steps)
 stepsbyinterval[maxrow,]$interval
 
@@ -25,7 +30,9 @@ colnames(activitynona)[2] <- 'steps'
 
 stepsbydatenona <- aggregate(steps ~ date, data=activitynona, FUN=sum)
 ggplot(stepsbydatenona, aes(x=date, y=steps)) +
-    geom_histogram(alpha=.5, stat="identity")
+    geom_histogram(alpha=.5, stat="identity") +
+    ylab('Number of Steps') +
+    xlab('Date')
 mean(stepsbydatenona$steps)
 median(stepsbydatenona$steps)
 
@@ -38,5 +45,8 @@ activitynona$daytype <- as.factor(activitynona$daytype)
 stepsbyintervalnona <- aggregate(steps ~ interval + daytype,
                                  data=activitynona, FUN=mean)
 ggplot(stepsbyintervalnona, aes(x=interval, y=steps)) +
-    geom_line() + facet_grid(daytype ~ .)
+    geom_line() +
+    facet_grid(daytype ~ .) +
+    ylab('Number of Steps') +
+    xlab('Interval')
 
