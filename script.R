03058f9e95ddde7e88dd56eb27bb4a12fc708348
herchu1 <hfoffani+datasciencecoursera@gmail.com>
2014-07-16 18:11:29 +0200

    Reorganize.

--- script.R
+++ script.R
@@ -1,9 +1,10 @@
 
+library(ggplot2)
 activity <- read.csv(unz('activity.zip', 'activity.csv'), stringsAsFactors = F)
 activity$date <- as.Date(activity$date)
-stepsbydate <- aggregate(steps ~ date, data=activity, FUN=sum)
 
-library(ggplot2)
+
+stepsbydate <- aggregate(steps ~ date, data=activity, FUN=sum)
 ggplot(stepsbydate, aes(x=date, y=steps)) + geom_histogram(alpha=.5, stat="identity")
 mean(stepsbydate$steps, na.rm=T)
 median(stepsbydate$steps, na.rm=T)
@@ -13,6 +14,7 @@ stepsbyinterval <- aggregate(steps ~ interval, data=activity, FUN=mean)
 ggplot(stepsbyinterval, aes(x=interval, y=steps)) + geom_line()
 maxrow <- stepsbyinterval$steps == max(stepsbyinterval$steps)
 stepsbyinterval[maxrow,]$interval
+
 
 sum(is.na(activity$steps))
 activitynona <- merge(activity, stepsbyinterval, 'interval')
